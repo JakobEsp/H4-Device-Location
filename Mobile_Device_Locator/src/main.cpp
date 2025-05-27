75ecfbcb5bc04a80cf2da345084336db4975f9af
Sebastian2510 <sebastian@dbvision.dk>
2025-05-27 12:38:51 +0200

    Add services to startup

--- Mobile_Device_Locator/src/main.cpp
+++ Mobile_Device_Locator/src/main.cpp
@@ -5,15 +5,16 @@
 #include "Services/NetworkService.h"
 // put function declarations here:
 
-void setup() {
-    Serial.begin(115200); // output serial to the serial monitor in terminal
-    Serial.println("Starting PSService...");
-    PromService::setup(); 
-    NetworkService::connect(); // connect to the network
-    WSClient::setup(); // setup the WebSocket client
+void setup()
+{
+  Serial.begin(115200); // output serial to the serial monitor in terminal
+  PromService::setup();
+  NetworkService::connect(); // connect to the network
+  WSClient::setup();         // setup the WebSocket client
 }
 
-void loop() {
+void loop()
+{
   PromService::incrementChannel();
   delay(1000); // wait for 1 second
   esp_wifi_set_channel(PromService::getChannel(), WIFI_SECOND_CHAN_NONE);

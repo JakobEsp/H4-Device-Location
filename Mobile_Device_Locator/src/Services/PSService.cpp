45035d18190216461dd122f275a5890e82f6aae6
Sebastian2510 <sebastian@dbvision.dk>
2025-05-27 12:45:31 +0200

    Set up sending data

--- Mobile_Device_Locator/src/Services/PSService.cpp
+++ Mobile_Device_Locator/src/Services/PSService.cpp
@@ -1,4 +1,7 @@
 #include "PSService.h"
+#include <Types/WSData.h>
+#include "WSClient.h"
+#include <Types/RSSIDistance.h>
 int PromService::channel = 1;
 void PromService::promiscuousSniffCallback(void *buf, wifi_promiscuous_pkt_type_t type)
 {
@@ -8,28 +11,14 @@ void PromService::promiscuousSniffCallback(void *buf, wifi_promiscuous_pkt_type_
     }
     wifi_promiscuous_pkt_t *pkt = (wifi_promiscuous_pkt_t *)buf;
 
-    // if (pkt->rx_ctrl.rssi <= -70)
-    // {
-    //     return;
-    // }
-
-    // Serial.printf("Management frame: %d bytes, RSSI: %d Payload: %u\n",
-    //               pkt->rx_ctrl.sig_len, pkt->rx_ctrl.rssi, pkt->payload);
-
-    uint8_t *payload = pkt->payload;
-
-    // The source MAC address is at byte offset 10 (802.11 header):
-    uint8_t *src_mac = &payload[10];
-    // The source MAC address is at byte offset 10 (802.11 header):
-    char macStr[18];
-    snprintf(macStr, sizeof(macStr), "%02X:%02X:%02X:%02X:%02X:%02X",
-             src_mac[0], src_mac[1], src_mac[2],
-             src_mac[3], src_mac[4], src_mac[5]);
-    if (strstr(macStr, "44") != nullptr)
+    if (pkt->rx_ctrl.rssi <= RSSIDistance::NEAR)
+    {
         return;
-    printf("Channel: %d Source MAC: %s\n", pkt->rx_ctrl.channel, macStr);
+    }
 
-    Serial.println();
+    WSData data(pkt);
+    Serial.printf("%s\n", data.toJson());
+    WSClient::send(data);
 }
 
 void PromService::setPromiscuousMode(bool enable)

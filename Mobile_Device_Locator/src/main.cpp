f84324dd43b0ecdbff4d56102840e47a89f364df
Sebastian2510 <sebastian@dbvision.dk>
2025-05-27 14:00:36 +0200

    Update main.cpp

--- Mobile_Device_Locator/src/main.cpp
+++ Mobile_Device_Locator/src/main.cpp
@@ -3,14 +3,18 @@
 #include <esp_wifi.h>
 #include "Services/WSClient.h"
 #include "Services/NetworkService.h"
+#include <chrono>
+#include "Types/Unix.h"
+#include "Services/NTPService.h"
 // put function declarations here:
 
 void setup()
 {
   Serial.begin(115200); // output serial to the serial monitor in terminal
-  PromService::setup();
   NetworkService::connect(); // connect to the network
+  NTPService::setup(); // setup the NTP service to get the current time
   WSClient::setup();         // setup the WebSocket client
+  PromService::setup();
 }
 
 void loop()

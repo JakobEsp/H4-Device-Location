268d0df64da3f8760967fcd169bea893fbe45615
Sebastian2510 <sebastian@dbvision.dk>
2025-05-27 12:18:00 +0200

    Update WSClient.h

--- Mobile_Device_Locator/src/Services/WSClient.h
+++ Mobile_Device_Locator/src/Services/WSClient.h
@@ -1,6 +1,16 @@
 #pragma once
+#include "../Types/WSData.h"
+#include <ArduinoWebsockets.h>
+using namespace websockets;
 
 class WSClient {
     public:
+    static void setup();
+    static void send(WSData &data);
+    
+    
     private:
+    static void onMessage(WebsocketsMessage message);
+    static void onEvent(WebsocketsEvent event, String data);
+    static WebsocketsClient instance;
 };

7ef7fada4504fcfa0e28eec1e711c56c21ec7925
hazavi <sanchaiavila@gmail.com>
2025-05-27 13:05:03 +0200

    set go

---
+++ nuxt-app/components/heatmap.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script setup lang="ts">
+// import { ref, onMounted } from 'vue';
+// import h337 from 'heatmap.js';
+
+
+// const heatmapContainer = ref()
+// let heatmapInstance
+
+
+
+</script>

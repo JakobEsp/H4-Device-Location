828c0fce8326da8bc624ae86be4f9e3047b06dab
JakobEsp <jakobespersen0@gmail.com>
2025-05-27 14:08:00 +0200

    aha

--- nuxt-app/server/routes/_ws.ts
+++ nuxt-app/server/routes/_ws.ts
@@ -1,16 +1,34 @@
+import { ref } from "vue";
 
+let numberOfRequests = 0;
+const num = ref(0);
+const wsReadings = "readings";
 
+const readings: Record<string, WebsocketData[]> = {}
 
 export default defineWebSocketHandler({
   open(peer) {
-    console.log("[ws] open", peer);
+    // console.log("[ws] open", peer);
   },
 
   message(peer, message) {
-    console.log("[ws] message", peer, message);
-    if (message.text().includes("ping")) {
-      peer.send("pong");
-    }
+    // save data until there is messages all esp's with the same id / mac in a reasonable timeframe 
+    // use Trilateration to get the position of the device based on esp 
+    // possibly transform the data to a more useful format
+    console.log("[ws] message", message);
+    num.value++;
+    console.log("[ws] num.value", num.value); 
+    numberOfRequests++;
+    console.log("[ws] numberOfRequests", numberOfRequests);
+
+    // const data: WebsocketData = JSON.parse(message.text());
+
+    // if (!readings[data.hwid]) {
+    //   readings[data.hwid] = [];
+    // }else {
+    //   console.log("No hardware id get good")
+    // }
+    // peer.publish(wsReadings, num.value);
   },
 
   close(peer, event) {
--- nuxt-app/shared/types.d.ts
+++ nuxt-app/shared/types.d.ts
@@ -3,8 +3,10 @@ declare interface WebsocketData {
   macAddress: string;
   channel: number;
   rssi: number;
+  rssi_type: string;
   sig_len: number;
   timestamp: number;
   time: string;
   hwid: string;
 }
+

6dcb30adfbe75c8fe442538ca4e674772894f317
JakobEsp <jakobespersen0@gmail.com>
2025-05-28 10:18:54 +0200

    change notes

--- nuxt-app/server/routes/_ws.ts
+++ nuxt-app/server/routes/_ws.ts
@@ -12,9 +12,11 @@ export default defineWebSocketHandler({
   },
 
   message(peer, message) {
+
     // save data until there is messages all esp's with the same id / mac in a reasonable timeframe 
-    // use Trilateration to get the position of the device based on esp 
-    // possibly transform the data to a more useful format
+    // use Trilateration to get the ca geolocation of the device based on esp geolocation
+    // Transform the data to x, y and id
+
     console.log("[ws] message", message);
     num.value++;
     console.log("[ws] num.value", num.value); 
